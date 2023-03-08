a9f1c1a71ba77427c68697d74aa1023a1cd2204c
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-07 21:23:15 -0300

    feat: Component Tasks.

--- todoList/src/App.tsx
+++ todoList/src/App.tsx
@@ -3,12 +3,14 @@ import { Todo } from './components/Todo';
 
 import './global.css';
 import styles from './App.module.css';
+import { Tasks } from './components/Tasks';
 
 export function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
       <Todo />
+      <Tasks />
     </div>
   );
 }
