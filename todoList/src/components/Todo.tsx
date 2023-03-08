7d406d18e94ba30da44cfed3b252b36ccc6cdfb5
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-07 21:09:14 -0300

    feat: Task interface, useState to add new tasks and handle tasks.

--- todoList/src/components/Todo.tsx
+++ todoList/src/components/Todo.tsx
@@ -1,15 +1,53 @@
 import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import styles from './Todo.module.css';
 
+interface Tasks {
+  id: number;
+  content: string;
+}
+
 export function Todo() {
+  const [tasks, setTasks] = useState<Tasks[]>([]);
+  const [newTask, setNewTask] = useState('');
+
+  console.log('tasks', tasks);
+
+  function handleNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length + 1,
+        content: newTask,
+      },
+    ]);
+
+    setNewTask('');
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
   return (
     <div className={styles.wrapper}>
-      <input type='text' placeholder='Adicione uma nova tarefa' />
-      <button>
-        Criar
-        <PlusCircle size={22} weight={'bold'} />
-      </button>
+      <form onSubmit={handleNewTask}>
+        <input
+          name='task'
+          value={newTask}
+          onChange={handleNewTaskChange}
+          type='text'
+          placeholder='Adicione uma nova tarefa'
+          required
+        />
+        <button>
+          Criar
+          <PlusCircle size={22} weight={'bold'} />
+        </button>
+      </form>
     </div>
   );
 }
