37a6d0aebb8408d3e498409126a663d13d47f141
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-08 22:12:34 -0300

    feat: Checkbox and others styles configuration.

--- todoList/src/components/Tasks.tsx
+++ todoList/src/components/Tasks.tsx
@@ -1,4 +1,6 @@
 import { Trash } from 'phosphor-react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+
 import styles from './Tasks.module.css';
 
 export function Tasks() {
@@ -6,17 +8,37 @@ export function Tasks() {
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.tasksNumbers}>
-          <span>Tarefas Criadas</span>
-          <span>Concluídas</span>
+          <span className={styles.tasksCreated}>
+            Tarefas Criadas <span className={styles.tasksCreatedAmount}>5</span>
+          </span>
+          <span className={styles.tasksDone}>
+            Concluídas <span className={styles.tasksDoneAmount}>2 de 5</span>
+          </span>
         </div>
-        <div>
+        <div className={styles.wrapperIndividualTask}>
           <div className={styles.individualTask}>
-            <input type='checkbox' />
+            <Checkbox.Root className={styles.checkboxRoot}>
+              <Checkbox.Indicator className={styles.checkboxindicator} />
+            </Checkbox.Root>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Necessitatibus numquam fugit deleniti.
             </p>
-            <Trash size={22} />
+            <div className={styles.iconIndividualTask}>
+              <Trash size={24} />
+            </div>
+          </div>
+          <div className={styles.individualTask}>
+            <Checkbox.Root className={styles.checkboxRoot}>
+              <Checkbox.Indicator />
+            </Checkbox.Root>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus numquam fugit deleniti.
+            </p>
+            <div className={styles.iconIndividualTask}>
+              <Trash size={24} />
+            </div>
           </div>
         </div>
       </div>
