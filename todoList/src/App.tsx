06d5766a66bd944d6e25d7b73d5fad4f95f6755b
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-13 21:43:16 -0300

    feat: Hard code some tasks.

--- todoList/src/App.tsx
+++ todoList/src/App.tsx
@@ -1,16 +1,41 @@
 import { Header } from './components/Header';
 import { Todo } from './components/Todo';
+import { Tasks, TaskType } from './components/Tasks';
 
 import './global.css';
 import styles from './App.module.css';
-import { Tasks } from './components/Tasks';
+
+const tasks: TaskType[] = [
+  {
+    id: 1,
+    task: 'Acordar cedo.',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Tomar Água',
+    completed: false,
+  },
+  {
+    id: 3,
+    task: 'Ler',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Estudar ReactJs',
+    completed: true,
+  },
+];
 
 export function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
       <Todo />
-      <Tasks />
+      {tasks.map((task) => {
+        return <Tasks key={task.id} tasks={task} />;
+      })}
     </div>
   );
 }
